0fba476b944229bbfbac4ace210ef1dda1b70551
QinXuYang <1446445040@qq.com>
2020-01-10 01:19:11 +0800

    优化代码结构，提取公共部分

--- js/battery2.js
+++ js/battery2.js
@@ -1,52 +1,50 @@
+const random  = function (min, max, floor) {
+	let num = min + Math.random() * (max - min)
+	if (floor) {
+		return Math.floor(num)
+	} else {
+		return num
+	}
+}
+
 const scale = 150
 class Circle {
-	constructor() {
-		this.width = scale //画布宽度
+	constructor () {
+		this.width = scale // 画布宽度
 		this.height = scale * 4/3 // 小球移动的最大高度
 		this.range = scale / 4 // 左右留下的间距
 		
-		this.update()
-		this.createCircle()
+		this.init()
 	}
-	update () {
-		// x初始横坐标位置
-		this.x = this.random(this.range, this.width - this.range)
-		this.vy = this.random(1500, 3000, true) // 用时间衡量速度
-		this.radius = this.random(15, 30)
-		
-		this.createCircle()
-	}
-	random (min, max, floor) {
-		let num = min + Math.random() * (max - min)
-		if (floor) {
-			return Math.floor(num)
-		} else {
-			return num
-		}
-	}
-	createCircle () {
+	create (x, radius) {
 		let c = document.createElement('div')
-		c.style.width = this.radius + 'px'
-		c.style.height = this.radius + 'px'
-		c.style.borderRadius = this.radius + 'px'
-		c.style.left = this.x - this.radius / 2 + 'px' // 中心点
+		c.style.width = radius + 'px'
+		c.style.height = radius + 'px'
+		c.style.borderRadius = radius + 'px'
+		c.style.left = x - radius / 2 + 'px' // 中心点
 		c.classList.add('circles')
-		this.dom = c
+		return c
+	}
+	init () {
+		this.radius = random(15, 30)
+		this.vy = random(1500, 3000, true) // 用时间衡量速度
+		this.x = random(this.range, this.width - this.range) // x初始横坐标位置
+		this.dom = this.create(this.x, this.radius)
 	}
 }
 class Run {
 	constructor() {
 		this.container = document.querySelector('.circle-container')
-		this.circles = new Set()
-		this.num = 10 // 小球个数
 		
-		this.createCircle()
+		this.circles = this.createCircle(10)
 		this.run()
 	}
-	createCircle () {
-		for (let i=0; i<this.num; i++) {
-			this.circles.add(new Circle())
+	createCircle (num) {
+		let circles = new Set()
+		for (let i=0; i<num; i++) {
+			circles.add(new Circle())
 		}
+		return circles
 	}
 	animation (circle) {
 		this.container.appendChild(circle.dom)
@@ -55,8 +53,7 @@ class Run {
 		}, {
 			duration: circle.vy,
 			complete: () => {
-				circle.dom.remove()
-				circle.update()
+				circle.init()
 				this.animation(circle)
 			}
 		})
