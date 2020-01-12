27c2427f54689a2c5065868a6f11bf5ca2f41638
QinXuYang <1446445040@qq.com>
2020-01-12 19:11:33 +0800

    去掉全局变量scale

--- js/battery2.js
+++ js/battery2.js
@@ -7,35 +7,35 @@ const random  = function (min, max, floor) {
 	}
 }
 
-const scale = 150
 class Circle {
 	constructor () {
-		this.width = scale // 画布宽度
-		this.height = scale * 4/3 // 小球移动的最大高度
-		this.range = scale / 4 // 左右留下的间距
-		
 		this.init()
 	}
-	create (x, radius) {
+	create () {
 		let c = document.createElement('div')
-		c.style.width = radius + 'px'
-		c.style.height = radius + 'px'
-		c.style.borderRadius = radius + 'px'
-		c.style.left = x - radius / 2 + 'px' // 中心点
+		c.style.width = this.radius + 'px'
+		c.style.height = this.radius + 'px'
+		c.style.borderRadius = this.radius + 'px'
+		c.style.left = this.x - this.radius / 2 + 'px' // 中心点
 		c.classList.add('circles')
 		return c
 	}
 	init () {
 		this.radius = random(15, 30)
 		this.vy = random(1500, 3000, true) // 用时间衡量速度
-		this.x = random(this.range, this.width - this.range) // x初始横坐标位置
-		this.dom = this.create(this.x, this.radius)
+		this.x = random(Circle.range, Circle.width - Circle.range) // x初始横坐标位置
+		this.dom = this.create()
 	}
 }
+
 class Run {
 	constructor() {
 		this.container = document.querySelector('.circle-container')
-		
+		// 设置静态量，描述小球画布范围
+		Circle.width = this.container.offsetWidth // 画布宽度
+		Circle.height = this.container.offsetHeight // 小球移动的最大高度
+		Circle.range = Circle.width / 4 // 左右留下的间距
+
 		this.circles = this.createCircle(10)
 		this.run()
 	}
@@ -48,8 +48,8 @@ class Run {
 	}
 	animation (circle) {
 		this.container.appendChild(circle.dom)
-		return circle.dom.velocity({
-			bottom: circle.height + 'px'
+		return Animate(circle.dom, {
+			bottom: Circle.height + 'px'
 		}, {
 			duration: circle.vy,
 			complete: () => {
